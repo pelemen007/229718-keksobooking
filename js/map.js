04284a3aadad4eed3e2b88bb2f6de5b3f951df47
Artsiom Pakholchyk <pelemen007@mail.ru>
2017-04-14 00:36:32 +0300

    Закончен п1, занимаюсь DOMом

--- js/map.js
+++ js/map.js
@@ -1,94 +1,102 @@
 'use strict';
 
+// Создание массива элементов
+
 var nearbyAds = [];
 
-  var randomInteger = function(min, max) {
-    return Math.floor(Math.random()*max)+min;
-  };
+var randomInteger = function (min, max) {
+  return Math.floor(Math.random() * max) + min;
+};
 
-  var getAvatar = function (id) {
-    return 'img/avatars/user0'+id+'.png';
-  };
+var getAvatar = function (id) {
+  return 'img/avatars/user0' + id + '.png';
+};
 
-  var typeTitle = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
-  var getTitle = function () {
-    var i = randomInteger(0, typeTitle.length - 1);
-    return typeTitle[i];
-  };
+var typeTitle = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+var getTitle = function () {
+  var i = randomInteger(0, typeTitle.length - 1);
+  return typeTitle[i];
+};
 
-  var typeHouse = ['flat', 'house', 'bungalo'];
-  var getTypeHouse = function () {
-    var i = randomInteger(0, typeHouse.length - 1);
-    return typeHouse[i];
-  };
+var typeHouse = ['flat', 'house', 'bungalo'];
+var getTypeHouse = function () {
+  var i = randomInteger(0, typeHouse.length - 1);
+  return typeHouse[i];
+};
 
-  var typeTime = ['12:00', '13:00', '14:00'];
-  var getTimeCheckin = function () {
-    var i = randomInteger(0, typeTime.length - 1);
-    return typeTime[i];
-  };
-  var getTimeCheckout = function () {
-    var i = randomInteger(0, typeTime.length - 1);
-    return typeTime[i];
-  };
+var typeTime = ['12:00', '13:00', '14:00'];
+var getTimeCheckin = function () {
+  var i = randomInteger(0, typeTime.length - 1);
+  return typeTime[i];
+};
+var getTimeCheckout = function () {
+  var i = randomInteger(0, typeTime.length - 1);
+  return typeTime[i];
+};
 
-  var typeFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-  var getFeatures = function () {
+var typeFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var getFeatures = function () {
 
-    var i = randomInteger(1, typeFeatures.length);             // Объявляем переменную длина нового массива и назначаем ей случайное число от 1 до длины typeFeatures
-    var newTypeFeatures = [];
-    newTypeFeatures.lenght = i;                                // Создаем новый массив этой длины
-      for (var j = 0; j < i; j++) {                            // Пробегаемся по новому массиву в цикле
-        var k = randomInteger(0, typeFeatures.length - 1);     // и на каждый шаг создаем новое случайное значение, и берем его из массива typeFeatures
+  var i = randomInteger(1, typeFeatures.length);
+  var newTypeFeatures = [];
+  for (var j = 0; j < i; j++) {
+    var k = randomInteger(0, typeFeatures.length - 1);
+    var chooseFeature = typeFeatures[k];
+    if (newTypeFeatures.indexOf(chooseFeature) <= -1) {
+      newTypeFeatures.push(typeFeatures[k]);
+    }
+  }
+  return newTypeFeatures;
+};
 
-          for (var l = 0; l < newTypeFeatures.length; l++) {
-            if (newTypeFeatures[l] === typeFeatures[k]) {      // Если такого значения еще нет в новом цикле, то кладем его туда
-              return false;
-            }
-          }
-          newTypeFeatures[j] = typeFeatures[k];
+var getLocation = function (locX, locY) {
+  return 'location.' + locX + ', location.' + locY + '';
+};
 
+for (var id = 0; id < 8; id++) {
+
+  var locationX = randomInteger(300, 900);
+  var locationY = randomInteger(100, 500);
+
+  var idAvatar = id + 1;
+
+  nearbyAds[id] = {
+    "author": {
+      "avatar": getAvatar(idAvatar)                   // строка, адрес изображения вида img/avatars/user{{xx}}.png, где xx это число от 1 до 8 с ведущим нулем. Например 01, 02 и т. д. Адреса изображений не повторяются
+    },
+    "offer": {
+      "title": getTitle(),                            // строка, заголовок предложения, одно из фиксированных значений "Большая уютная квартира", "Маленькая неуютная квартира", "Огромный прекрасный дворец", "Маленький ужасный дворец", "Красивый гостевой домик", "Некрасивый негостеприимный домик", "Уютное бунгало далеко от моря", "Неуютное бунгало по колено в воде". Значения не должны повторяться.
+      "address": getLocation(locationX, locationY),   // строка, адрес предложения, представляет собой запись вида "{{location.x}}, {{location.y}}"
+      "price": randomInteger(1000, 1000000),          // число, случайная цена от 1000 до 1 000 000
+      "type": getTypeHouse(),                         // строка с одним из трех фиксированных значений: flat, house или bungalo
+      "rooms": randomInteger(1, 5),                   // число, случайное количество комнат от 1 до 5
+      "guests": randomInteger(1, 20),                 // число, случайное количество гостей, которое можно разместить
+      "checkin": getTimeCheckin(),                    // строка с одним из трех фиксированных значений: 12:00, 13:00 или 14:00
+      "checkout": getTimeCheckout(),                  // строка с одним из трех фиксированных значений: 12:00, 13:00 или 14:00
+      "features": getFeatures(),                      // массив строк случайной длины из ниже предложенных: "wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"
+      "description": '',                              // пустая строка
+      "photos": []                                    // пустой массив
+    },
+    "location": {
+      "x": locationX,                                 // случайное число, координата x метки на карте в блоке .tokyo__pin-map от 300 до 900,
+      "y": locationY                                  // случайное число, координата y метки на карте в блоке .tokyo__pin-map от 100 до 500
     }
   };
-
-  var getLocation = function (locationX, locationY) {
-    return 'location.'+locationX+', location.'+locationY+'';
-  };
-
-  for (var id = 0; id < 8; id++) {
-
-    var locationX = randomInteger(300, 900);
-    var locationY = randomInteger(100, 500);
-
-    var idAvatar = id + 1;
-
-    nearbyAds[id] = {
-      "author": {
-        "avatar": getAvatar(idAvatar)                          //строка, адрес изображения вида img/avatars/user{{xx}}.png, где xx это число от 1 до 8 с ведущим нулем. Например 01, 02 и т. д. Адреса изображений не повторяются
-      },
-      "offer": {
-        "title": getTitle(),                            // строка, заголовок предложения, одно из фиксированных значений "Большая уютная квартира", "Маленькая неуютная квартира", "Огромный прекрасный дворец", "Маленький ужасный дворец", "Красивый гостевой домик", "Некрасивый негостеприимный домик", "Уютное бунгало далеко от моря", "Неуютное бунгало по колено в воде". Значения не должны повторяться.
-        "address": getLocation(),                       //строка, адрес предложения, представляет собой запись вида "{{location.x}}, {{location.y}}"
-        "price": randomInteger(1000, 1000000),          //число, случайная цена от 1000 до 1 000 000
-        "type": getTypeHouse(),                         //"type": строка с одним из трех фиксированных значений: flat, house или bungalo
-        "rooms": randomInteger(1, 5),                   //число, случайное количество комнат от 1 до 5
-        "guests": randomInteger(1, 20),                 //число, случайное количество гостей, которое можно разместить
-        "checkin": getTimeCheckin(),                    //строка с одним из трех фиксированных значений: 12:00, 13:00 или 14:00
-        "checkout": getTimeCheckout(),                  //строка с одним из трех фиксированных значений: 12:00, 13:00 или 14:00
-        "features": getFeatures(),                      //массив строк случайной длины из ниже предложенных: "wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"
-        "description": '',                              //пустая строка
-        "photos": []                                    //пустой массив
-      },
-      "location": {
-        "x": locationX,                                 //случайное число, координата x метки на карте в блоке .tokyo__pin-map от 300 до 900,
-        "y": locationY                                  //случайное число, координата y метки на карте в блоке .tokyo__pin-map от 100 до 500
-      }
-    };
-  }
+}
 
 // Создание DOM элементов
 
+
+for (var i = 0; i < 8; i ++) {
+  var offer = nearbyAds[i].offer;
   var pin = document.querySelector('.pin');
-  pin.style.left = offer.location.x;
-  pin.style.top = offer.location.y;
-  pin.img('src', offer.author.avatar);
+  pin.style.left = location.x + 'px';
+  pin.style.top = location.y + 'px';
+  pin.appendChild(img);
+}
+
+   // return pin;
+  // .cloneNode(true)
+  // pin.img('src', offer.author.avatar);
+
+
